7a2c65deecc85bc15e4e8efe1f487668682355d8
Nikolay Arhipov <n@arhipov.net>
2023-01-25 20:20:58 +0200

    Fixed nvidia-uvm-reload
    A single dbus call may spawm multiple messages, of which the middle one
    can be the one we need. So we have to deplete the whole queue on each
    iteration.

--- nvidia-uvm-reload/main.c
+++ nvidia-uvm-reload/main.c
@@ -27,49 +27,48 @@ int main(void) {
   printf("Starting dbus listener\n");
   while (true) {
     dbus_connection_read_write(conn, -1);
+
     DBusMessage *msg = dbus_connection_pop_message(conn);
+    while (msg != NULL) {
+      if (dbus_message_is_method_call(msg, "org.powertools", "RestartNvidia")) {
+        printf("Restarting nvidia module\n");
+        int res = system("rmmod nvidia_uvm");
+        res |= system("modprobe nvidia_uvm");
+        printf("Returning\n");
 
-    if (NULL == msg) {
-      continue;
-    }
-
-    printf("New message!\n");
-    if (dbus_message_is_method_call(msg, "org.powertools", "RestartNvidia")) {
-      printf("Restarting nvidia module\n");
-      int res = system("rmmod nvidia_uvm");
-      res |= system("modprobe nvidia_uvm");
-      printf("Returning\n");
-
-      DBusMessage *reply = dbus_message_new_method_return(msg);
-      if (!dbus_connection_send(conn, reply, NULL)) {
-        fprintf(stderr, "Unable to reply\n");
-        return 1;
-      }
-
-      printf("Exit status is %i\n", res);
-      if (res == 0) {
-        DBusMessage *msg2 =
-            dbus_message_new_signal("/", "org.powertools", "NvidiaRestarted");
-        if (NULL == msg2) {
-          fprintf(stderr, "NvidiaRestarted message is null\n");
+        DBusMessage *reply = dbus_message_new_method_return(msg);
+        if (!dbus_connection_send(conn, reply, NULL)) {
+          fprintf(stderr, "Unable to reply\n");
           return 1;
         }
 
-        // send the message and flush the connection
-        if (!dbus_connection_send(conn, msg2, NULL)) {
-          fprintf(stderr, "Unable to reply with a signal\n");
-          return 1;
+        printf("Exit status is %i\n", res);
+        if (res == 0) {
+          DBusMessage *msg2 =
+              dbus_message_new_signal("/", "org.powertools", "NvidiaRestarted");
+          if (NULL == msg2) {
+            fprintf(stderr, "NvidiaRestarted message is null\n");
+            return 1;
+          }
+
+          // send the message and flush the connection
+          if (!dbus_connection_send(conn, msg2, NULL)) {
+            fprintf(stderr, "Unable to reply with a signal\n");
+            return 1;
+          }
+
+          dbus_message_unref(msg2);
         }
 
-        dbus_message_unref(msg2);
+        dbus_connection_flush(conn);
+        dbus_message_unref(reply);
       }
 
-      dbus_connection_flush(conn);
-      dbus_message_unref(reply);
-    }
+      // free the message
+      dbus_message_unref(msg);
 
-    // free the message
-    dbus_message_unref(msg);
+      msg = dbus_connection_pop_message(conn);
+    }
   }
 
   return EXIT_SUCCESS;
